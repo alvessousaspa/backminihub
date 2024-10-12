73ff0ee1946634447ffd92c59a4df49e39a47ea7
alvessousaspa <80459995+alvessousaspa@users.noreply.github.com>
2024-10-12 18:40:32 -0300

    fsd

--- src/app.js
+++ src/app.js
@@ -3,8 +3,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const todosRouter = require('./routes/todos');
-
 const app = express();
 
 app.use(logger('dev'));
@@ -12,9 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '..', 'public')));
-
-// Routes
-app.use('/todos', todosRouter);
 
 
 module.exports = app;
